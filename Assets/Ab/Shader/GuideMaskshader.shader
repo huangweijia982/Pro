3c82f67f329d4705036176c1f31abc1d664c4535
13750018475 <1115236192@qq.com>
2020-05-19 17:37:00 +0800

    新手引导

---
+++ Assets/Ab/Shader/GuideMaskshader.shader
@@ -0,0 +1,145 @@
+﻿Shader "UI/GuideMask"
+{
+	Properties
+	{
+		[PerRendererData] _MainTex("Sprite Texture", 2D) = "white" {}
+		_Color("Tint", Color) = (1,1,1,1)
+		_Blur("边缘虚化的范围", Range(0,100)) = 100
+		_R("圆的半径R", Range(0,1)) = 0.5
+		_StencilComp("Stencil Comparison", Float) = 8
+		_Stencil("Stencil ID", Float) = 0
+		_StencilOp("Stencil Operation", Float) = 0
+		_StencilWriteMask("Stencil Write Mask", Float) = 255
+		_StencilReadMask("Stencil Read Mask", Float) = 255		
+		_ColorMask("Color Mask", Float) = 15
+		//中心
+		_Origin("Circle",Vector) = (0,0,0,0)
+		//裁剪方式 0圆形 1圆形
+		_MaskType("Type",Float) = 0		
+	}
+
+		SubShader
+	{
+		Tags
+	{
+		"Queue" = "Transparent"
+		"IgnoreProjector" = "True"
+		"RenderType" = "Transparent"
+		"PreviewType" = "Plane"
+		"CanUseSpriteAtlas" = "True"
+	}
+
+		Stencil
+	{
+		Ref[_Stencil]
+		Comp[_StencilComp]
+		Pass[_StencilOp]
+		ReadMask[_StencilReadMask]
+		WriteMask[_StencilWriteMask]
+	}
+
+		Cull Off
+		Lighting Off
+		ZWrite Off
+		ZTest[unity_GUIZTestMode]
+		Blend SrcAlpha OneMinusSrcAlpha
+		ColorMask[_ColorMask]
+
+		Pass
+	{
+		Name "Default"
+		CGPROGRAM
+#pragma vertex vert
+#pragma fragment frag
+#pragma target 2.0
+
+#include "UnityCG.cginc"
+#include "UnityUI.cginc"
+
+
+
+		struct appdata_t
+	{
+		float4 vertex : POSITION;
+		float4 color : COLOR;
+		float2 texcoord : TEXCOORD0;
+		UNITY_VERTEX_INPUT_INSTANCE_ID
+	};
+
+	struct v2f
+	{
+		float4 vertex : SV_POSITION;
+		fixed4 color : COLOR;
+		float2 texcoord : TEXCOORD0;
+		float4 worldPosition : TEXCOORD1;
+		UNITY_VERTEX_OUTPUT_STEREO
+	};
+
+	fixed4 _Color;
+	fixed4 _TextureSampleAdd;
+	float4 _ClipRect;
+	float4 _Origin;
+	float _MaskType;	
+	float _Blur;
+	float _R;
+	v2f vert(appdata_t IN)
+	{
+		v2f OUT;
+		UNITY_SETUP_INSTANCE_ID(IN);
+		UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(OUT);
+		OUT.worldPosition = IN.vertex;
+		OUT.vertex = UnityObjectToClipPos(OUT.worldPosition);
+		OUT.texcoord = IN.texcoord;
+		OUT.color = IN.color * _Color;
+		return OUT;
+	}
+
+	sampler2D _MainTex;
+
+	fixed4 frag(v2f IN) : SV_Target
+	{
+		half4 color = (tex2D(_MainTex, IN.texcoord) + _TextureSampleAdd) * IN.color;
+
+		if (_MaskType == 0) {
+			if (distance(IN.worldPosition.xy, _Origin.xy) <= _Origin.z)
+			{
+				color.a = 0;
+			}			
+		}
+		else if (_MaskType == 1) {
+			//UnityGet2DClipping这个函数实现了判断2D空间中的一点是否在一个矩形区域中
+			if (UnityGet2DClipping(IN.worldPosition.xy, _Origin))
+			{
+				color.a = 0;
+			}
+		}
+		else if (_MaskType == 2)
+		{
+			if (UnityGet2DClipping(IN.worldPosition.xy, _Origin))
+			{
+				color.a = 0;
+				//#ifdef UNITY_UI_CLIP_RECT
+                //color.a *= UnityGet2DClipping(IN.worldPosition.xy, _Origin);
+                ///#endif
+ 
+                //#ifdef UNITY_UI_ALPHACLIP
+                    //clip(color.a - 0.001);
+                    //#endif
+ 
+                    //圆形
+                    //float x = IN.texcoord.x-0.5f;
+                    //float y = IN.texcoord.y-0.5f;
+                    //float lerp = (1 - _Blur * (_R*_R - (x * x + y * y)))*color.a;//如果不加这句代码，图片边缘会有明显的锯齿
+                    //color.a = (color.a - lerp)*step(x*x + y * y, _R*_R);//如果 _R*_R<(x*x + y * y)，返回 0；否则，返回 1。
+
+			}               
+ 
+
+		}
+
+		return color;
+	}
+		ENDCG
+	}
+	}
+}
---
+++ Assets/Scripts/Manager/GuideMask.cs
@@ -0,0 +1,206 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GuideMask : MonoBehaviour {
+    [SerializeField] RawImage _rawImage; //遮罩图片
+    [SerializeField] RectTransform _rectTrans;
+    Material _materia;
+    Canvas _canvas;
+    private EventPenetrate ev;
+
+    public static GuideMask Instance
+    {
+        private set;
+        get;
+    }
+
+    void Awake()
+    {
+
+        Instance = this;
+        _materia = _rawImage.material;
+        ev = transform.GetComponent<EventPenetrate>();
+    }
+    /// <summary>
+    /// 创建圆形点击区域
+    /// </summary>
+    /// <param name="target">目标位置</param>
+    /// <param name="offeset">大小偏移量</param>
+    /// <param name="CallBack">点击的回调</param>
+    public void CreateCircleMask(GameObject target, float offset)
+    {
+        if (target != null && ev != null)
+        {
+            RectTransform rec = target.GetComponent<RectTransform>();
+            CreateCircleMask(GetTargetCenter(rec), GetTargetRad(rec) + offset);
+            ev.SetTargetImage(target);
+        }
+    }
+
+
+    /// <summary>
+    /// 创建圆形点击区域
+    /// </summary>
+    /// <param name="target">目标位置</param>
+    /// <param name="offeset">大小偏移量</param>
+    /// <param name="CallBack">点击的回调</param>
+    public void CreateCircleMask(GameObject target, float offset,float x,float y)
+    {
+        if (target != null && ev != null)
+        {
+            RectTransform rec = target.GetComponent<RectTransform>();
+            CreateCircleMask(GetTargetCenter(rec, x, y), GetTargetRad(rec) + offset);
+            ev.SetTargetImage(target);
+        }
+    }
+
+
+    /// <summary>
+    /// 创建圆形点击区域
+    /// </summary>
+    /// <param name="pos">圆心的屏幕位置</param>
+    /// <param name="rad">圆的半径</param>
+    /// <param name="CallBack">点击的回调</param>
+    public void CreateCircleMask(Vector3 pos, float rad)
+    {
+        ShowGuideMask();
+        _rectTrans.sizeDelta = Vector2.zero;
+        _materia.SetFloat("_MaskType", 0f);
+        _materia.SetVector("_Origin", new Vector4(pos.x, pos.y, rad, 20));
+    }
+
+    /// <summary>
+    /// 创建矩形点击区域
+    /// </summary>
+    /// <param name="obj">目标位置</param>
+    /// <param name="CallBack">回调</param>
+    public void CreateRectangleMask(GameObject target)
+    {
+        if (target != null && ev != null)
+        {
+            RectTransform rec = target.GetComponent<RectTransform>();
+            Vector3[] _corners = new Vector3[4];
+            rec.GetWorldCorners(_corners);
+            Vector2 pos1 = WorldToCanvasPos(_corners[0]);//选取左下角
+            Vector2 pos2 = WorldToCanvasPos(_corners[2]);//选取右上角
+            CreateRectangleMask(pos1, pos2);
+            ev.SetTargetImage(target);
+        }
+    }
+
+    /// <summary>
+    /// 创建矩形点击区域
+    /// </summary>
+    /// <param name="pos">矩形的屏幕位置</param>
+    /// <param name="pos1">左下角位置</param>
+    /// <param name="pos2">右上角位置</param>
+    /// <param name="CallBack">回调</param>
+    public void CreateRectangleMask(Vector3 pos1, Vector3 pos2)
+    {
+        ShowGuideMask();
+        _rectTrans.sizeDelta = Vector2.zero;
+        _materia.SetFloat("_MaskType", 1.0f);
+        _materia.SetVector("_Origin", new Vector4(pos1.x, pos1.y, pos2.x, pos2.y));
+    }
+
+    /// <summary>
+    /// 获取对象RectTransform的半径
+    /// </summary>
+    /// <param name="rect"></param>
+    /// <returns></returns>
+    public float GetTargetRad(RectTransform rect)
+    {
+        Vector3[] _corners = new Vector3[4];
+        rect.GetWorldCorners(_corners);
+        //计算最终高亮显示区域的半径       
+        float _radius = Vector2.Distance(WorldToCanvasPos(_corners[0]),
+                     WorldToCanvasPos(_corners[2])) / 2f;
+        return _radius;
+    }
+
+    /// <summary>
+    /// 获取对象RectTransform的中心点
+    /// </summary>
+    /// <param name="rect"></param>
+    /// <returns></returns>
+    public Vector2 GetTargetCenter(RectTransform rect)
+    {
+        Vector3[] _corners = new Vector3[4];
+        rect.GetWorldCorners(_corners);
+
+        float x = _corners[0].x + ((_corners[3].x - _corners[0].x) / 2f);
+        float y = _corners[0].y + ((_corners[1].y - _corners[0].y) / 2f);
+        Vector3 centerWorld = new Vector3(x, y, 0);
+        Vector2 center = WorldToCanvasPos(centerWorld);
+        return center;
+    }
+    /// <summary>
+    /// 获取对象RectTransform的中心点
+    /// </summary>
+    /// <param name="rect"></param>
+    /// <returns></returns>
+    public Vector2 GetTargetCenter(RectTransform rect, float _x, float _y)
+    {
+        Vector3[] _corners = new Vector3[4];
+        rect.GetWorldCorners(_corners);
+
+        float x = _corners[0].x + ((_corners[3].x - _corners[0].x) / 2f);
+        float y = _corners[0].y + ((_corners[1].y - _corners[0].y) / 2f);
+        Vector3 centerWorld = new Vector3(x + _x, y + _y, 0);
+        Vector2 center = WorldToCanvasPos(centerWorld);
+        return center;
+    }
+
+
+    /// <summary>
+    /// 世界坐标向画布坐标转换
+    /// </summary>
+    /// <param name="world">世界坐标</param>
+    /// <returns>返回画布上的二维坐标</returns>
+    private Vector2 WorldToCanvasPos(Vector3 world)
+    {
+        if (null == _canvas) _canvas = transform.GetComponentInParent<Canvas>();
+        Vector2 position;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(_canvas.transform as RectTransform,
+            world, _canvas.GetComponent<Camera>(), out position);
+        return position;
+    }
+    public  void ShowGuideMask()
+    {
+        if (_rawImage != null &&!_rawImage.enabled)
+        {
+            _rawImage.enabled = true;
+        }
+    }
+    /// <summary>
+    /// 将可点击区域设置为空
+    /// </summary>
+    public void SetTargetNil()
+    {
+        if (ev != null)
+        {
+            ev.SetTargetImage(null);
+        }
+    }
+
+    /// <summary>
+    /// 关闭引导遮罩
+    /// </summary>
+    public void CloseGuideMask()
+    {
+        if (_rawImage != null)
+        {
+            _rawImage.enabled = false;
+        }
+    }
+    public void OnDestroy()
+    {
+#if UNITY_EDITOR
+        if (_materia != null)
+        {
+            _materia.SetFloat("_MaskType", 1.0f);
+            _materia.SetVector("_Origin", new Vector4(0, 0, 0, 0));
+        }
+#endif
+    }
+}
