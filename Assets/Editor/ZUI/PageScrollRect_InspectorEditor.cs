1e4c547f14afa228b783f12c6cc3a65a3f66187e
13750018475 <1115236192@qq.com>
2020-06-08 10:51:08 +0800

    翻页ScrollView

---
+++ Assets/Editor/ZUI/PageScrollRect_InspectorEditor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.UI;
+[CustomEditor(typeof(PageScrollRect))]
+public class PageScrollRect_InspectorEditor : ScrollRectEditor
+{
+    private PageScrollRect ps;
+    private SerializedObject obj;
+    private SerializedProperty pageNum;
+    private SerializedProperty loadItemMode;
+    private SerializedProperty slidMode;
+    private SerializedProperty movementType;
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        ps = (PageScrollRect)target;
+        obj = new SerializedObject(target);
+        pageNum = obj.FindProperty("pageNum");
+        loadItemMode = obj.FindProperty("loadItemMode");
+        slidMode = obj.FindProperty("slidMode");
+        movementType = obj.FindProperty("_movementType");
+    }
+    public override void OnInspectorGUI()
+    {
+        ps.prePageBtn = (Button)EditorGUILayout.ObjectField("prePageBtn",ps.prePageBtn,typeof(Button));
+        ps.nextPageBtn = (Button)EditorGUILayout.ObjectField("nextPageBtn", ps.nextPageBtn, typeof(Button));
+        ps.pageTxt = (Text)EditorGUILayout.ObjectField("pageTxt", ps.pageTxt, typeof(Text));
+        ps.contentGG = (GridLayoutGroup)EditorGUILayout.ObjectField("contentGG", ps.contentGG, typeof(GridLayoutGroup));
+        ps.content = (RectTransform)EditorGUILayout.ObjectField("content", ps.content, typeof(RectTransform));
+        EditorGUILayout.PropertyField(slidMode);
+       
+        ps.pageSize = EditorGUILayout.FloatField("pageSize", ps.pageSize);
+        ps.slidSpeed= EditorGUILayout.FloatField("slidSpeed",ps.slidSpeed);
+        ps.dragSensitive= EditorGUILayout.Slider(new GUIContent("dragSensitive"), ps.dragSensitive, 0, 1);
+        EditorGUILayout.PropertyField(loadItemMode);
+        if(loadItemMode.enumValueIndex==0) EditorGUILayout.PropertyField(pageNum);
+        obj.ApplyModifiedProperties();
+        EditorGUILayout.PropertyField(movementType);
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+        }
+    }
+   
+}
---
+++ Assets/Scripts/ZUI/DoubleScrollRect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DoubleScrollRect : ScrollRect, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    public ScrollRect parentScroll;
+
+    protected override void Start()
+    {
+        base.Start();
+        parentScroll = transform.parent.parent.parent.parent.GetComponent<ScrollRect>();
+    }
+
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        base.OnBeginDrag(eventData);
+        parentScroll.OnBeginDrag(eventData);
+    }
+    public override void OnDrag(PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+        parentScroll.OnDrag(eventData);
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        base.OnEndDrag(eventData);
+        parentScroll.OnEndDrag(eventData);
+    }
+
+
+}
